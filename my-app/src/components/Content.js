94f98acc2aea386581adf0a59971a02f81a44ec0
R Moruju <morujurazvan@gmail.com>
2023-05-05 11:30:32 +0000

    Use component lifecycles metod to set a timeout function

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react'
 import css from "./css/Content.module.css"
 import { savedPosts } from "../posts.json"
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            isLoaded: false
         }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 3000)
     }
 
     render() {
@@ -18,8 +27,9 @@ export class Content extends Component {
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                 </div>
-                <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                <div className={css.SearchResults}> {
+                    this.state.isLoaded ? (<PostItem savedPosts={savedPosts} />) : (<Loader />)
+                    }
                 </div>
 
             </div>
